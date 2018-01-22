bf5ea8646457157226ac10fe2edcefaad9babe93
Delink-D <delinkdesigns@gmail.com>
2018-01-23 00:04:26 +0300

    setup of the firebase configuration to the component

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,19 +1,40 @@
 <template>
   <div class="home">
-    
+    <span v-for="book in localFirebase.books">
+      book - {{book.name}}
+    </span>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HomeComponent',
-  data () {
-    return {
-      
+  import environment from '../../config/env'
+  import firebase from 'firebase'
+
+  /** Initialize Firebase */
+  let config = {
+    apiKey: environment.firebase.apiKey,
+    authDomain: environment.firebase.authDomain,
+    databaseURL: environment.firebase.databaseURL,
+    projectId: environment.firebase.projectId,
+    storageBucket: environment.firebase.storageBucket,
+    messagingSenderId:environment.firebase.messagingSenderId 
+  };
+
+  let app = firebase.initializeApp(config);
+  let db = app.database();
+  let booksRef = db.ref('books');
+
+  export default {
+    name: 'HomeComponent',
+    data () {
+      return {
+        localFirebase: {
+          books: booksRef
+        }
+      }
     }
   }
-}
-</script>
+</script> 
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
